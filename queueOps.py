64b354cd34a338b3a72728207eb05597d7f09cb3
Scikous <95752605+Scikous@users.noreply.github.com>
2023-10-10 21:35:18 +0300

    Added priority queue

--- queueOps.py
+++ queueOps.py
@@ -1,31 +1,27 @@
-
-import numpy as np
-import queue
-
 class queueObj():
     def __init__(self, maxsize=None):
             self.queue = []
             self.maxsize = maxsize
-    
-    def add(self, item: any):
+
+    def add(self, item):
         if(self.maxsize is None or (len(self.queue) < self.maxsize)):
             self.queue.append(item)
         else:
             raise ValueError("Queue be full")
-        
+            
     def FifoGet(self):
         if(len(self.queue) > 0):
             item = self.queue[0]
             self.queue.pop(0)
             return item
+        
     def LifoGet(self):
         if(len(self.queue) > 0):
             lastIndex = len(self.queue)-1
             item = self.queue[lastIndex]
             self.queue.pop(lastIndex)
             return item
-    
-
+        
     def queue(self):
         return self.queue
     
@@ -50,14 +46,78 @@ class queueObj():
             raise ValueError("No item to swap with")
 
 
+class priorityQueueObj(queueObj):
+    def __init__(self, maxsize=None):
+            self.queue = []
+            super().__init__(maxsize)
+
+    def add(self, item: any, **kwargs):
+        if(len(kwargs) == 1 and kwargs["priorityNum"]):
+            priorityNum = kwargs["priorityNum"]
+        else:
+            priorityNum = len(self.queue)
+        
+        def _sortQueue(queue):
+            for ind, item in enumerate(queue):
+                if(ind < len(queue)-1):
+                    nextItemPriorityNum = queue[ind+1][0]
+                    #if next item higher priority than current
+                    if(nextItemPriorityNum < item[0]):
+                        storeItem = queue[ind+1]
+                        queue[ind+1] = item
+                        queue[ind] = storeItem
+                        return _sortQueue(queue) #without this, only moves item by one
+            
+        if(self.maxsize is None or (len(self.queue) < self.maxsize)):
+            #given priorityNum cannot be already taken
+            if(any(num[0] == priorityNum for num in self.queue)):
+                raise ValueError("Priority value already reserved")                
+            self.queue.append([priorityNum, item])
+
+            if(len(self.queue) > 1):
+                _sortQueue(self.queue)
+        else:
+            raise ValueError("Queue be full")
+    
+    def priorityGet(self, priorityNum):
+        if(len(self.queue) > 0):
+            for item in self.queue:
+                if(item[0] == priorityNum):
+                    return item[1]
+            raise ValueError("Could not find item based on given priorityNum")
+        else:
+            raise ValueError("Queue has no items")
+        
+    #priority queue has no reason to have access to this function
+    def swapWithFront(self, index: int):
+        raise ValueError("Function is restricted to only work with queueObj types")
+
+q = queueObj(maxsize=5)
+q2 = priorityQueueObj(maxsize=7)
+
+q2.add(1, priorityNum=7)
+q2.add(35, priorityNum=3)
+q2.add(3, priorityNum=5)
+q2.add(4, priorityNum=4)
+q2.add(6, priorityNum=12)
+q2.add(219, priorityNum=8)
+q2.add(21, priorityNum=2)
+
+print("\nPriority Queue:",q2.queue)
+
+#q2.swapWithFront(4)
+#q2.priorityGet(0)
+print(f"State of queue post swap: {q2.queue}")
+print(f"Priority Queue FIFO and LIFO Gets: FIFO: {q2.FifoGet()}, LIFO: {q2.LifoGet()}, priorityGET: {q2.priorityGet(3)}")
+print(f"Final priority queue: {q2.queue}, Full check: {q2.fullCheck()}, Empty check: {q2.emptyCheck()}\n")
 
 
 
-q = queueObj(maxsize=3)
-q.add(1)
+
 q.add("hello")
 q.add([])
-#q.add((1,3,"4"))
+q.add((1,3,"4"))
+q.add("lol")
 
 print("Queue:",q.queue)
 q.swapWithFront(2)
@@ -65,16 +125,4 @@ q.swapWithFront(1)
 print(f"State of queue post swap: {q.queue}")
 print(f"Queue FIFO and LIFO Gets: FIFO: {q.FifoGet()}, LIFO: {q.LifoGet()}")
 
-print(q.queue, q.fullCheck(), q.emptyCheck())
-
-q2 = queue.Queue()
-print(q2.queue)
-print(q2.full())
-q2.put(1)
-q2.put(2)
-q2.put(3)
-q2.empty()
-
-print(q2.empty())
-# q.get()
-# print(q.queue)
+print(f"Final queue: {q.queue}, Full check: {q.fullCheck()}, Empty check: {q.emptyCheck()}\n")
