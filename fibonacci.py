aba3265df8b35b3a0aa5ef9e4bafe4921ce4afff
Scikous <rozeusker@gmail.com>
2023-12-30 09:06:08 +0200

    Improved and fixed functions

--- fibonacci.py
+++ fibonacci.py
@@ -3,95 +3,162 @@ import matplotlib.pyplot as plt
 import time
 import concurrent.futures as cf
 
-def time_func(fun, n):
-    startTime  = time.time()
-    fun(n)
-    endTime  = time.time()
-    
-    execTime = endTime - startTime
-    return execTime
+def time_func(fun, fib_val):
+    start_time  = time.time()
+    val = fun(fib_val)
+    end_time  = time.time()
+    exec_time = end_time - start_time
+    return exec_time, val
 
-#O(2^n)
-def trib(n):#presume n >= 1
-    if n == 0:
-        raise ValueError("n must be >= 1")
-    elif n <= 2:#if n 1 or 2 then sequence is 1,1 -> n = 1
-        return 1 #c2.5 = 1
-    else: #(n-1)+n | 1,1 -> 1+1=2 -> 1,1,2...
-        val = trib(n-1) + trib(n-2)    
-    return val
-
-def trib_steps(n):
-    if n == 0:
-        raise ValueError("n must be >= 1") 
-    elif n <= 2:
+#O(2^N)
+def trib_slowest(fib_val):#presume fib_val >= 1
+    if fib_val < 0:
+        raise ValueError("fib_val must be >= 0")
+    elif fib_val == 0:
+        return 0
+    elif fib_val == 1:
         return 1
+    else: #(fib_val-1)+(fib_val-2) -> 1,1 -> 1+1=2 -> 1,1,2...
+        return trib_slowest(fib_val-1) + trib_slowest(fib_val-2)  
+      
+#O(2^N)
+def trib_steps_slowest(fib_val, steps=0):
+    if fib_val == 0 or fib_val == 1:
+        return steps+1
     else:
-        steps = trib_steps(n-1) + trib_steps(n-2)
-    return steps
+        steps_left = trib_steps_slowest(fib_val - 1, steps)
+        steps_right = trib_steps_slowest(fib_val - 2, steps)
+        return steps_left + steps_right + 1
+    
+#O(2^(N-1))
+def trib_slow(fib_val):#presume fib_val >= 1
+    if fib_val <= 0:
+        raise ValueError("fib_val must be >= 1")
+    elif fib_val == 1 or fib_val == 2:#if fib_val 1 or 2 then sequence is 1,1 -> fib_val = 1
+        return 1
+    else: #(fib_val-1)+fib_val | 1,1 -> 1+1=2 -> 1,1,2...
+        return trib_slow(fib_val-1) + trib_slow(fib_val-2) 
+
+#O(2^(N-1))
+def trib_steps_slow(fib_val, steps=0):
+    if fib_val == 1 or fib_val == 2:
+        return steps+1
+    else:
+        steps_left = trib_steps_slow(fib_val - 1, steps)
+        steps_right = trib_steps_slow(fib_val - 2, steps)
+        return steps_left + steps_right + 1
 
 #O(N)
-def trib_for(n):
-    if n == 0:
-        raise ValueError("n must be >= 1")  
-    fibSeqList = []
-    
-    for val in range (0, n):
-        if(len(fibSeqList) > 1): 
-            val = fibSeqList[val-1] + fibSeqList[val-2]
+def trib(fib_val, traversed=None):#presume fib_val >= 1
+    if traversed is None:
+        traversed = {}
+    if fib_val <= 0:
+        raise ValueError("fib_val must be >= 1")
+    elif fib_val <= 2:#if fib_val 1 or 2 then sequence is 1,1 -> fib_val = 1
+        return 1
+    elif fib_val not in traversed: #(fib_val-1)+fib_val | 1,1 -> 1+1=2 -> 1,1,2...
+        traversed[fib_val] = trib(fib_val-1, traversed) + trib(fib_val-2,traversed)
+    return traversed[fib_val]
+
+#O(N)
+def trib_steps_fast(fib_val, traversed=None, steps=0):
+    if traversed is None:
+        traversed = []
+    if fib_val <= 0:
+        raise ValueError("fib_val must be >= 1")
+    elif fib_val <= 2:
+        return steps+1
+    elif fib_val not in traversed:
+        traversed.append(fib_val)
+        steps = trib_steps_fast(fib_val-1, traversed, steps)
+        steps = trib_steps_fast(fib_val-2, traversed, steps)
+    return steps+1
+
+#O(N)
+def trib_for(fib_val):
+    if fib_val <= 0:
+        raise ValueError("fib_val must be >= 1")  
+    fib_seq_list = []
+    for val in range(0, fib_val):
+        if(val > 1): 
+            val = fib_seq_list[val-1] + fib_seq_list[val-2]
         else:
             val = 1
-        fibSeqList.append(val)
-    return fibSeqList
+        fib_seq_list.append(val)
+    return fib_seq_list[-1]
 
-def trib_for_steps(n):
-    return n
-
-def timed_trib(n):
-    return time_func(trib, n)
-
-def timed_trib_for(n):
-    return time_func(trib_for, n)
+def trib_for_steps(fib_val):
+    return fib_val
 
 
-def execute_in_parallel(func, seq):
+
+def timed_trib_recursive_slowest(fib_val):
+    return time_func(trib_slowest, fib_val)
+
+def timed_trib_recursive_slow(fib_val):
+    return time_func(trib_slow, fib_val)
+
+def timed_trib_recursive_fast(fib_val):
+    return time_func(trib, fib_val)
+
+def timed_trib_for(fib_val):
+    return time_func(trib_for, fib_val)
+
+
+def execute_fib_in_parallel(func, seq: list):
     with cf.ProcessPoolExecutor() as executor:
         results = list(executor.map(func, seq))
     return results
 
-
 if __name__ == '__main__':
-    stepsTrib = []
-    stepsTribFor = []
-    timedFor = []
-    timedRec = []
+    fib_get_values = 37  # Adjust as needed
+    fib_seq_to_get = [fib_val for fib_val in range(1, fib_get_values + 1)]
+    #get Fibonacci sequence values and their execution times
+    timed_recursive_slowest, fib_vals_recursive_slowest = zip(*execute_fib_in_parallel(timed_trib_recursive_slowest, fib_seq_to_get))
+    timed_recursive_slow, fib_vals_recursive_slow = zip(*execute_fib_in_parallel(timed_trib_recursive_slow, fib_seq_to_get))
+    timed_recursive_fast, fib_vals_recursive_fast = zip(*execute_fib_in_parallel(timed_trib_recursive_fast, fib_seq_to_get))
+    timed_for, fib_vals_for = zip(*execute_fib_in_parallel(timed_trib_for, fib_seq_to_get))
 
-    fibToGet = 37  # Adjust as needed
-    fibSeqToGet = [n for n in range(1, fibToGet + 1)]
-    startTime  = time.time()
-        
-    timedRec = execute_in_parallel(timed_trib, fibSeqToGet)
-    timedFor = execute_in_parallel(timed_trib_for, fibSeqToGet)
-    stepsTrib =  execute_in_parallel(trib_steps, fibSeqToGet)
-    stepsTribFor =  execute_in_parallel(trib_for_steps, fibSeqToGet)
+    #get Fibonacci sequence execution steps
+    steps_trib_recursive_slowest =  execute_fib_in_parallel(trib_steps_slowest, fib_seq_to_get)
+    steps_trib_recursive_slow =  execute_fib_in_parallel(trib_steps_slow, fib_seq_to_get)
+    steps_trib_recursive_fast =  execute_fib_in_parallel(trib_steps_fast, fib_seq_to_get)
+    steps_trib_for =  execute_fib_in_parallel(trib_for_steps, fib_seq_to_get)
     
-    endTime  = time.time()
+    print(f"Recursive O(2^N) fib vals: {fib_vals_recursive_slowest}\fib_val\nRecursive O(2^(N-1)) fib vals: {fib_vals_recursive_slow}\fib_val\nRecursive O(N) fib vals: {fib_vals_recursive_fast}\fib_val\nFor loop O(N) fib vals: {fib_vals_for}\fib_val")
+    print("--------------------------------")
+    print(f"Recursive O(2^N) fib times: {timed_recursive_slowest}\fib_val\nRecursive O(2^(N-1)) fib times: {timed_recursive_slow}\fib_val\nRecursive O(N) fib times: {timed_recursive_fast}\fib_val\nFor loop O(N) fib times: {timed_for}\fib_val")
+    print("--------------------------------")
+    print(f"Recursive O(2^N) fib steps: {steps_trib_recursive_slowest}\fib_val\nRecursive O(2^(N-1)) fib steps: {steps_trib_recursive_slow}\fib_val\nRecursive O(N) fib steps: {steps_trib_recursive_fast}\fib_val\nFor loop O(N) fib steps: {steps_trib_for}")
 
-    execTime = endTime - startTime
 
-    print(execTime)
     plt.subplot()
-    plt.scatter(fibSeqToGet, timedRec, color='red')
-    plt.scatter(fibSeqToGet, timedFor, color='blue')
+    plt.scatter(fib_seq_to_get, timed_recursive_slowest, color='orange', label='Recursive 2^N')
+    plt.scatter(fib_seq_to_get, timed_recursive_slow, color='red', label='Recursive 2^(N-1)')
+    plt.scatter(fib_seq_to_get, timed_recursive_fast, color='green', label='Recursive N')
+    plt.scatter(fib_seq_to_get, timed_for, color='blue', label='Using For Loop')
     plt.xlabel('Fibonacci value to calculate')
     plt.ylabel('Execution Time (seconds)')
-    plt.title('Execution Time vs. Fibonacci')
+    plt.title('Execution Time (Fibonacci)')
+    plt.legend()
 
+    #recursive_fast and For loop values are too similar, and cannot be distinguished from one another unless zoomed in
     plt.figure()
-    plt.scatter(fibSeqToGet, stepsTrib, color='red')
-    plt.scatter(fibSeqToGet, stepsTribFor, color='blue')
+    plt.scatter(fib_seq_to_get, steps_trib_recursive_slowest, color='orange', label='Recursive 2^N')
+    plt.scatter(fib_seq_to_get, steps_trib_recursive_slow, color='red', label='Recursive 2^(N-1)')
+    plt.scatter(fib_seq_to_get, steps_trib_recursive_fast, color='green', label='Recursive N')
+    plt.scatter(fib_seq_to_get, steps_trib_for, color='blue', label='Using For Loop')
     plt.xlabel('Fibonacci value to calculate')
     plt.ylabel('Steps Taken')
-    plt.title('Steps taken vs. Fibonacci')
-    plt.show()
+    plt.title('Steps taken (Fibonacci)')
+    plt.legend()
 
+    plt.figure()
+    plt.scatter(fib_seq_to_get, steps_trib_recursive_fast, color='green', label='Recursive N')
+    plt.scatter(fib_seq_to_get, steps_trib_for, color='blue', label='Using For Loop')
+    plt.xlabel('Fibonacci value to calculate')
+    plt.ylabel('Steps Taken')
+    plt.title('Steps taken (Fibonacci)')
+    plt.legend()
+    
+    plt.show()
